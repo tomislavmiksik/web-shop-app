6742b87c0d944846d96458f2cda5670c3ed2085f
Tomislav Mikšik <64797760+tomislavmiksik@users.noreply.github.com>
2021-10-06 08:15:59 +0200

    cart screen formatting

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -3,52 +3,54 @@ import 'package:provider/provider.dart';
 import 'package:web_shop_app/providers/cart_provider.dart';
 
 class CartScreen extends StatelessWidget {
-
   static const routePath = 'cart-screen';
 
   @override
   Widget build(BuildContext context) {
-
     final cart = Provider.of<Cart>(context);
     return Scaffold(
       backgroundColor: Color(0xFF1f306e),
       appBar: AppBar(
         actions: [
           Card(
-          color: Color(0xFF1f306e),
-          shadowColor: Colors.transparent,
-          //margin: EdgeInsets.all(15),
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                const Text(
-                  'Total',
-                  style: TextStyle(
-                    fontSize: 20,
-                    fontWeight: FontWeight.bold,
-
+            color: Color(0xFF1f306e),
+            shadowColor: Colors.transparent,
+            //margin: EdgeInsets.all(15),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  const Text(
+                    'Total',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                ),
-                const SizedBox(
-                  width: 10,
-                ),
-                Chip(
-                  label: Text('\$${cart.totalAmount}', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
-                  backgroundColor: Color(0xFFf5487f),
-                )
-              ],
+                  const SizedBox(
+                    width: 10,
+                  ),
+                  Chip(
+                    label: Text(
+                      '\$${cart.totalAmount}',
+                      style: TextStyle(
+                          color: Colors.white, fontWeight: FontWeight.bold),
+                    ),
+                    backgroundColor: Color(0xFFf5487f),
+                  )
+                ],
+              ),
             ),
           ),
-        ),],
+        ],
       ),
       //extendBodyBehindAppBar: true,
       body: Column(
         children: [
           Container(
-            //child:
-          ),
+              //child:
+              ),
         ],
       ),
     );
